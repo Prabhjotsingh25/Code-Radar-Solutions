e67bafb721c01ea55a6a39ad3b781abddf7f67ef
Prabhjotsingh25 <prabhjot1571.becse24@chitkara.edu.in>
2025-02-03 16:49:19 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=0){
+        Printf("Eligible");
+    } else{
+        Printf("Not Eligible")
+    }
+    return 0;
+}
